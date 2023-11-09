9c1f4b1ac601dcadb33763a217d574be051bcce5
Kimiya Espahbodi <148192904+Kimiyacode@users.noreply.github.com>
2023-11-09 02:25:55 +0100

    Update COVID cases and deaths.sql

--- COVID cases and deaths.sql
+++ COVID cases and deaths.sql
@@ -80,6 +80,7 @@ join PortfolioProject..CovidVaccinations vac
 
 -- Let's now look at the total amount of people that have been vaccinatied each day in each country
 
+	
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 
 From PortfolioProject..CovidDeaths dea
@@ -89,15 +90,30 @@ join PortfolioProject..CovidVaccinations vac
 where dea.continent is not null and vac.new_vaccinations is not null
 order by 2,3
 
+
+	
 -- I want to do a rolling count that sums up tha vaccinated and adds them up, resulting in a count function
-
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-SUM(CONVERT(int, vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.Location, dea.Date) as RollingPeopleVaccinated
---(Rolling_people_vaccinated/population)*100
-
+	
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
 From PortfolioProject..CovidDeaths dea
-join PortfolioProject..CovidVaccinations vac
+Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
-where dea.continent is not null and vac.new_vaccinations is not null
-order by 2
+where dea.continent is not null 
+)
+Select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
+From PopvsVac
+
+
+-- Here we get the % of total cases based on the population in each country
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
